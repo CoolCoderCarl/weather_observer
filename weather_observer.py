c1b7ca108e4ba182d659b30eedaf82f3888c826e
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-08-01 20:35:28 +0300

    Add func for getting info about weather

--- weather_observer.py
+++ weather_observer.py
@@ -1,30 +1,34 @@
 import requests
 from dotenv import load_dotenv
 import os
+from typing import Dict
 
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 
-country = "tr"
+# country = "tr"
+# city = "antalya"
 
-city = "antalya"
-
-target_dict = {"tr" :["antalya", "istambul", "ankara", "izmir"],
-                "esp": ["barcelona", "madrid", "vigo", "santander", "malaga"]}
+target_dict = {"tr": ["antalya", "istambul", "ankara", "izmir"],
+               "esp": ["barcelona", "madrid", "vigo", "santander", "malaga"]}
 
 
-# Send request return json string
-def get_weather_info(target_dict: dict):
-    # r = requests.get(f"https://api.weatherbit.io/v2.0/current?city={city}&country={country}&key={API_KEY}")
-    # return r.json()["data"][0]
-    for k, v in target_dict.items():
-        for i in v:
-            print(k, i)
+def get_weather_info(country: str, city: str) -> Dict:
+    try:
+        r = requests.get(f"https://api.weatherbit.io/v2.0/current?city={city}&country={country}&key={API_KEY}")
+        return r.json()["data"][0]
+    except requests.exceptions.RequestException:
+        pass
 
 # Build dict of required info
 
 # Reported in file
 
+
 if __name__ == '__main__':
-    print(get_weather_info(target_dict))
+    for country, cities in target_dict.items():
+        for city in cities:
+            get_weather_info(country, city)
+
+    # print(get_weather_info(target_dict))
